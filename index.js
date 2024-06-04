b7bf8d8d7f3571c53d470293b828271d75f75bda
AfrideHossain <asafridehossain171@gmail.com>
2024-06-04 19:13:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { connectMongo } = require("./mongoDbConnection");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+//call mongodb connection function
+connectMongo();
+
+// cors and cors configuration
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+};
+app.use(cors(corsConfig));
+app.options("", cors(corsConfig));
+
+//json middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Bolly-critics going good...");
+});
+
+/* // auth route
+app.use("/", require("./routes/authentication/auth"));
+// manage classes route
+app.use("/", require("./routes/manageClasses/manageClasses"));
+// manage users route
+app.use("/", require("./routes/manageUsers/manageUsers"));
+// manage payments route
+app.use("/", require("./routes/managePayments/managePayments")); */
+
+app.listen(port, () => {
+  console.log(`Bolly-critics listening on port ${port}`);
+});
